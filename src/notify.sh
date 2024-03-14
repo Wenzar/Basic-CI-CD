fd6c64999949d03ef726e818cdce31bbbb9d2d49
wenzar <grindelf@PC2>
2024-03-14 18:20:21 +0300

    .

--- src/notify.sh
+++ src/notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TELEGRAM_API_TOKEN="6967275596:AAFdVF4YQZ1D_tp-yfy0UHM2WVj68yl87_4"
-CHAT_ID="4186178789"
+CHAT_ID="323815524"
 
 URL="https://api.telegram.org/bot$TELEGRAM_API_TOKEN/sendMessage"
 TEXT="Deploy status: $1%0AProject name: $CI_PROJECT_NAME%0AURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/"
