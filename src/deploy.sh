4496becf1d7cea5a8c22602a141fd99922de1161
wenzar <grindelf@PC2>
2024-03-15 15:39:32 +0300

    .

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,18 +4,18 @@ REMOTE_HOST="172.31.41.206"
 REMOTE_USER="grindelf"
 REMOTE_DIR="/usr/local/bin"
 
-scp -o StrictHostKeyChecking=no cat/s21_cat grep/s21_grep $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+scp -o StrictHostKeyChecking=no cat/s21_cat grep/s21_grep $REMOTE_USER@$REMOTE_HOST:/tmp/
 
 if [ $? -ne 0 ]; then
-  echo "Ошибка при копировании файлов"
+  echo "Ошибка при копировании файлов!"
   exit 1
 fi
 
-# ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST "mv /tmp/s21_cat $REMOTE_DIR; mv /tmp/s21_grep $REMOTE_DIR"
+ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST "mv /tmp/s21_cat $REMOTE_DIR; mv /tmp/s21_grep $REMOTE_DIR"
 
-# if [ $? -ne 0 ]; then
-#   echo "Ошибка при перемещении файлов"
-#   exit 1
-# fi
+if [ $? -ne 0 ]; then
+  echo "Ошибка при перемещении файлов"
+  exit 1
+fi
 
-echo "Файлы успешно скопированы!"
+echo "Файлы успешно скопированы"
